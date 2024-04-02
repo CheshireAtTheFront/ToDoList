dc25b0d599b0ef333067a772a1c383619bfb2a1d
Diana Makarova <din_m-v@mail.ru>
2024-04-02 13:25:39 +0300

    localStorage пока не работает, над этим нужно еще по работать

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
-// todo-control
-// header-input
-// todo-list
-// todo-completed
 const todoControl = document.querySelector('.todo-control');
 const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 
-const toDoData = [];
+let toDoData = [];
 
+// для работы с элементами
 const render = function() {
 	todoList.innerHTML = '';
 	todoCompleted.innerHTML = '';
 
+	toDoData = getData();
+
 	toDoData.forEach(function(item) {
 		const li = document.createElement('li');
-
+		
 		li.classList.add('todo-item');
 
 		li.innerHTML = '<span class="text-todo">' + item.text + '</span>' + 
@@ -24,28 +23,50 @@ const render = function() {
 				'<button class="todo-complete"></button>' +
 				'</div>';
 
+		li.querySelector('.todo-complete').addEventListener('click', function() {
+			item.completed = !item.completed;
+			
+			render();
+		});
+
+		li.querySelector('.todo-remove').addEventListener('click', function() {
+			const itemIndex = toDoData.indexOf(item);
+			toDoData.splice(itemIndex, 1);
+
+			render();
+		});
+
 		if(item.completed) {
 			todoCompleted.append(li);
 		} else {
 			todoList.append(li);
 		}
-
-		li.querySelector('.todo-complete').addEventListener('click', function() {
-			item.completed = !item.completed
-			render();
-		})
 	})
 }
 
 todoControl.addEventListener('submit', function(event) {
-  event.preventDefault();
+	event.preventDefault();
 
 	const newToDo = {
 		text: headerInput.value,
 		completed: false,
 	}
-	toDoData.push(newToDo);
-	headerInput.value = '';
-
+	
+	if(!newToDo.text) {
+		return false
+	} else {
+		toDoData.push(newToDo);
+		headerInput.value = '';
+		setData();
+	}
 	render();
-})
+});
+
+// сохраняет элементы
+const setData = function() {
+	localStorage.setItem('todo', JSON.stringify(toDoData));
+}
+// возращает массив данных
+const getData = function() {
+	return JSON.parse(localStorage.getItem('todo')) || [];
+}
